ebc8cb49f04bf60564ceb585dd0c84b81569cad9
Fablizio <gats200@hotmail.it>
2021-11-29 18:01:41 +0100

    feat:add error function

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+/**Api Fetch */
 let weather = {
   apiKey: "ce23df9993022bcc0de19bafae0815b5",
   fetchWeather: function (city) {
@@ -7,37 +8,54 @@ let weather = {
         " &units=metric&appid=" +
         this.apiKey
     )
-      .then((response) => response.json())
+    /**check error 404*/
+        .then((response) => {
+            if (response.status === 404) {
+               
+                document.querySelector(".city").innerText = `${city} Is not Found`;
+            } else {
+                return response.json();
+            }
+        })
+
       .then((data) => this.displayWeather(data));
   },
+
+  /**Funcion with inner text for create the weater card */
   displayWeather: function (data) {
     const { name } = data;
-    const { icon, description } = data.weather [0];
-      const { temp, humidity, temp_min, temp_max, feels_like } = data.main;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity, temp_min, temp_max, feels_like } = data.main;
     const { speed } = data.wind;
-    const { cod } = data;
+    
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".temp-min").innerText = "Minimum Temperature: " + temp_min + "°C";
-      document.querySelector(".temp-max").innerText = "Maximum Temperature: " + temp_max + "°C";
-      document.querySelector(".feels-like").innerText = "Perceived Temperature: " + feels_like + "°C";
+    document.querySelector(".temp-min").innerText =
+      "Minimum Temperature: " + temp_min + "°C";
+    document.querySelector(".temp-max").innerText =
+      "Maximum Temperature: " + temp_max + "°C";
+    document.querySelector(".feels-like").innerText =
+      "Perceived Temperature: " + feels_like + "°C";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = "Wind speed " + speed + "km/h";
   },
+
+  /** Search Function */
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
-
   },
 };
 
+/**event listener click*/
 document.querySelector(".search").addEventListener("click", function () {
   weather.search();
 });
 
+/**event listener keyup-enter */
 document
   .querySelector(".search-bar")
   .addEventListener("keyup", function (event) {
@@ -46,6 +64,5 @@ document
     }
   });
 
-
-
+/**Default city */
 weather.fetchWeather("Palermo");
